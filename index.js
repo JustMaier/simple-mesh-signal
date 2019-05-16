4b817e83935bbc4652876cd69829ac2190c8bc53
Justin Maier <Zipp425@gmail.com>
2019-05-15 19:20:39 -0700

    Update WS and add ws proxy

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const { WrappedSocketServer, wrapSocket } = require('./WrappedSocketServer');
 
 class SimpleMeshSignal {
-    constructor(options){
-        const wss = new WrappedSocketServer(options);
+    constructor({pingInterval = 10000, ...wsOptions}){
+        const wss = new WrappedSocketServer(wsOptions);
         
         wss.on('connection', socket => {
             socket = wrapSocket(socket);
+            socket.alive = true;
         
+            // WebRTC
             socket.wrapper.on('discover', ({ peerName }) => {
                 socket.peerName = peerName;
                 wss.broadcast('discovered', { peerName }, x=>x.peerName !== peerName);
@@ -18,7 +20,31 @@ class SimpleMeshSignal {
         
                 peerSocket.wrapper.send('signal', { peerName: socket.peerName, signal });
             });
+
+            // WSS Proxy
+            socket.wrapper.on('data', ({ peerName, id, type, payload }) => {
+                const peerSocket = wss.findPeer(peerName);
+                if(!peerSocket || !socket.peerName) return;
+
+                peerSocket.wrapper.send('data', { peerName: socket.peerName, id, type, payload });
+            });
+
+            // Connection tracking
+            socket.wrapper.on('ping', () => {
+                socket.alive = true;
+            })
         });
+
+        setInterval(() => {
+            wss.clients.forEach(socket => {
+                if(!socket.alive){
+                    socket.terminate();
+                    if(socket.peerName) wss.broadcast('disconnected', {peerName: socket.peerName});
+                }
+
+                socket.alive = false;
+            });
+        }, pingInterval)
     }
 }
 
